f45da9f97c0878aa7cfaaf4a80cc323951d50a3f
MelkiorMisiak <misiakmelkior@gmail.com>
2026-08-31 11:08:52 -0300

    Estética

--- src/buscapadraoweb/Main.java
+++ src/buscapadraoweb/Main.java
@@ -176,6 +176,15 @@ public class Main {
 			System.out.println();
 		}
 
+    for(int i=0; i<alfabeto.length; i++){
+      System.out.printf("%2c ", alfabeto[i]);
+    }	
+		System.out.println();
+    for(int i=0; i<alfabeto.length; i++){
+      System.out.printf("---");
+    }	
+		System.out.println();
+
 		for(int i=0; i<estados.length; i++){
 			for(int j=0; j<alfabeto.length; j++){
 				System.out.printf("%2d ", matriz[i][j]);
